38109902721a519c227fd43640a1796f3202d3b8
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-08 11:59:51 +0530

    [Kartikeya] UC4

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ namespace TicTacToe_Program
         {
             char[] board = CreateBoard();
             char player = PlayerInput();
-            ShowBoard();
+            ShowBoard(board);
+            MakeMove(board, player);
         }
         public static char[] CreateBoard()
         {
@@ -33,14 +34,23 @@ namespace TicTacToe_Program
             }
             return player;
         }
-        public static void ShowBoard()
+        public static void ShowBoard(char [] board)
         {
-            char[] board = CreateBoard();
             Console.WriteLine("  {0}   |   {1}   |   {2}   ", board[1], board[2], board[3]);
-            Console.WriteLine("-----------------------------");
+            Console.WriteLine("---------------------------");
             Console.WriteLine("  {0}   |   {1}   |   {2}    ",board[4], board[5], board[6]);
-            Console.WriteLine("-----------------------------");
+            Console.WriteLine("---------------------------");
             Console.WriteLine("  {0}   |   {1}   |   {2}    ", board[7], board[8], board[9]);
+        }
+        public static void MakeMove(char[] board, char player)
+        {
+            Console.WriteLine("Enter the place you want to mark your move from 1 to 9");
+            int userIndex = Convert.ToInt32(Console.ReadLine());
+            if (board[userIndex] == ' ')
+                board[userIndex] = player;
+            else
+                Console.WriteLine("Place already occupied");
+            ShowBoard(board);
         }
     }
 }
