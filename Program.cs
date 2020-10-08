4f9663b793cd35ca9c104fb380153a0e92e0301f
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-08 10:33:21 +0530

    UC1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TicTacToe_Program
+{
+    class TicTacToeGame
+    {
+        static void Main(string[] args)
+        {
+            CreateBoard();
+            char playerLetter = PlayerInput();
+            char computerLetter = PlayerInput();
+            Console.WriteLine(playerLetter);
+            Console.WriteLine(computerLetter);
+        }
+        public static void CreateBoard()
+        {
+            char[] board = new char[10];
+            for(int i=1; i<board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            
+        }
+        public static char PlayerInput()
+        {
+            char player = 'X';
+            char computer = 'O';
+            return player;
+            return computer;
+        }
+    }
+}
